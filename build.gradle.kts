fef2009833daade87b08b287d1519da8c9cd45ff
valvenya <chenghiskhan2000@gmail.com>
2022-04-11 02:57:38 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val koin_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.pokebase"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    //Ktor dependencies
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-resources:$ktor_version")
+
+    //Koin dependencies
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
+
+    //Testing dependencies
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.insert-koin:koin-test:$koin_version")
+    testImplementation("io.insert-koin:koin-test-junit4:$koin_version")
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions {
+        jvmTarget = "13"
+    }
+}
---
+++ src/main/kotlin/com/pokebase/Application.kt
@@ -0,0 +1,16 @@
+package com.pokebase
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+
+    routing {
+        get("/test") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
